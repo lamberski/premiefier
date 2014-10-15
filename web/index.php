de4ae3de21d1972622c5f720a163667ef75d8fdd
Maciej Lamberski <maciej@lamberski.com>
2014-10-15 23:40:32 +0200

    Implement searching and displaying movie info

--- web/index.php
+++ web/index.php
@@ -2,14 +2,30 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-$app = new Silex\Application();
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+
+$app = new Application();
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
 
-$app->get('/', function () use ($app) {
-  return 'Hello world!';
+$app->get('/', function (Application $app, Request $request) {
+
+    $title = $request->query->get('title');
+    $movie = false;
+
+    if ($title) {
+        $url = 'http://www.omdbapi.com/?' . http_build_query(array('t' => $title));
+        $json = file_get_contents($url);
+        $movie = json_decode($json);
+    }
+
+    return $app['twig']->render('actions/index.twig', array(
+        'title' => $title,
+        'movie' => $movie,
+    ));
 });
 
 $app->run();
