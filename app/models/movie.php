9df9f0c3c7423aa476877ae7708b1dbfdeb3005b
Maciej Lamberski <maciej@lamberski.com>
2015-01-17 00:19:45 +0100

    Throw exceptions directly from Movie model if any error occurs

--- app/models/movie.php
+++ app/models/movie.php
@@ -4,30 +4,37 @@ namespace Premiefier;
 
 class Movie {
   public $title, $plot, $genre, $poster, $releasedAt;
-  public $wasFound, $wasAlreadyReleased;
 
   public function __construct($title) {
     $this->fetch($title);
   }
 
   protected function fetch($title) {
+    if (!$title) {
+      throw new \Exception('Enter movie title.');
+    }
+
     // Get movie data from IMDB API
     $url = 'http://www.omdbapi.com/?'.http_build_query(['t' => $title]);
     $json = file_get_contents($url);
     $data = json_decode($json);
-
     $data = $this->parseOMDBData($data);
 
-    // Save movie info in the object
+    // Assign movie info to the object
     $this->title = $data->Title;
     $this->plot = $data->Plot;
     $this->genre = $data->Genre;
     $this->poster = $data->Poster;
     $this->releasedAt = $data->Released;
 
-    // Save flags values
-    $this->wasFound = $this->title != '';
-    $this->wasAlreadyReleased = $this->releasedAt && $this->releasedAt < time();
+    // Detect any errors
+    if (!$this->title) {
+      throw new \Exception('Movie was not found.');
+    } elseif (!$movie->releasedAt) {
+      throw new \Exception('Release date of this movie is not available.');
+    } elseif ($this->releasedAt && $this->releasedAt < time()) {
+      throw new \Exception('This movie was already released.');
+    }
   }
 
   protected function parseOMDBData($data) {
@@ -37,7 +44,7 @@ class Movie {
     }
 
     // Set date from text
-    if ($data->Released) {
+    if (!empty($data->Released)) {
       $data->Released = strtotime($data->Released);
     }
 
