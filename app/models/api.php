98d09044cd72d53b3d0c94b720415a63d58895d7
Maciej Lamberski <maciej@lamberski.com>
2015-03-26 22:36:03 +0100

    Sort movie premieres so first are ones that can be subscribed to

--- app/models/api.php
+++ app/models/api.php
@@ -6,12 +6,13 @@ class API {
 
   public static function getMoviesByTitle($title)
   {
-    $query = http_build_query(['apikey' => getenv('API_KEY'), 'q' => $title, 'page_limit' => 10]);
-    $url   = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?';
-    $json  = file_get_contents($url.$query);
-    $data  = json_decode($json, true);
+    $query  = http_build_query(['apikey' => getenv('API_KEY'), 'q' => $title, 'page_limit' => 10]);
+    $url    = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?';
+    $json   = file_get_contents($url.$query);
+    $data   = json_decode($json, true);
+    $movies = array_map('Premiefier\Models\API::examineReleaseDate', $data['movies']);
 
-    return array_map('Premiefier\Models\API::examineReleaseDate', $data['movies']);
+    return self::sortByReleaseDate($movies);
   }
 
   public static function getMovieByID($id)
@@ -37,6 +38,15 @@ class API {
     }
 
     return $movie;
+  }
+
+  protected static function sortByReleaseDate($movies)
+  {
+    usort($movies, function ($movie) {
+      return ($movie['already_released'] || empty($movie['release_dates']['theater'])) ? -1 : 1;
+    });
+
+    return array_reverse($movies);
   }
 
 }
