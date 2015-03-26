8f9978708001a8288489a53b67e147f5c68c3e6a
Maciej Lamberski <maciej@lamberski.com>
2015-03-26 20:35:13 +0100

    Add fancy animation of removing subscriptions

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -41,156 +41,154 @@
 
   };
 
-  var Application = {
+  var Subscribe = {
 
-    /**
-     * Initialize features
-     */
     init: function () {
-      Application.Unsubscribe.init();
-      Application.Movies.init();
-      Application.Movie.init();
+      if (Elements.containers.search.length === 0) return;
+
+      Elements.containers.search.html(Helpers.compileTemplate('search'));
+      Subscribe.bindSearchForm();
+      Subscribe.bindSubscribeForm();
+      Subscribe.bindTogglingMovieDetails();
     },
 
-    /**
-     * Features for Movies Search page
-     */
-    Movies: {
-      init: function () {
-        if (Elements.containers.search.length === 0) return;
+    bindSearchForm: function () {
+      Elements.body.on('submit', '#search-container form', function (event) {
+        event.preventDefault();
 
-        Elements.containers.search.html(Helpers.compileTemplate('search'));
-        Application.Movies.bindSearchForm();
-        Application.Movies.bindSubscribeForm();
-      },
+        var form = $(this);
 
-      bindSearchForm: function () {
-        Elements.body.on('submit', '#search-container form', function (event) {
-          event.preventDefault();
+        Helpers.changeButtonState(form, 'processing', true);
+        Elements.containers.movies.addClass('is-loading');
 
-          var form = $(this);
-          Helpers.changeButtonState(form, 'processing', true);
-          Elements.containers.movies.addClass('is-loading');
-
-          $.ajax({
-            url: form.attr('action'),
-            type: form.attr('method'),
-            data: form.serialize()
-          })
-          .always(function (data) {
-            data = data.responseJSON || data;
-            Elements.containers.search.html(Helpers.compileTemplate('search', data));
-            Elements.containers.movies
-              .html(Helpers.compileTemplate('movies', data))
-              .removeClass('is-loading');
-          });
+        $.ajax({
+          url: form.attr('action'),
+          type: form.attr('method'),
+          data: form.serialize()
+        })
+        .always(function (data) {
+          data = data.responseJSON || data;
+          Elements.containers.search
+            .html(Helpers.compileTemplate('search', data));
+          Elements.containers.movies
+            .html(Helpers.compileTemplate('movies', data))
+            .removeClass('is-loading');
         });
-      },
-
-      bindSubscribeForm: function () {
-        Elements.body.on('submit', '.movie__form form', function (event) {
-          event.preventDefault();
-
-          var form = $(this);
-          var movie = form.closest('.movie');
-          var container = form.closest('.movie__form');
-
-          Helpers.changeButtonState(form, 'processing', true);
-
-          $.ajax({
-            url: form.attr('action'),
-            type: form.attr('method'),
-            data: form.serialize()
-          })
-          .always(function (data) {
-            data = data.responseJSON || data;
-            container.html(Helpers.compileTemplate('subscribe', data));
-          });
-        });
-      }
+      });
     },
 
-    /**
-     * Features for Unsubscribe page
-     */
-    Unsubscribe: {
-      init: function () {
-        if (Elements.containers.unsubscribe.length === 0) return;
+    bindSubscribeForm: function () {
+      Elements.body.on('submit', '.movie__form form', function (event) {
+        event.preventDefault();
 
-        Elements.containers.unsubscribe.html(Helpers.compileTemplate('unsubscribe'));
-        Application.Unsubscribe.bindSearchForm();
-        Application.Unsubscribe.bindUnbscribeForm();
-      },
+        var form = $(this);
 
-      bindSearchForm: function () {
-        Elements.body.on('submit', '#unsubscribe-container form', function (event) {
-          event.preventDefault();
+        var movie = form.closest('.movie');
+        var container = form.closest('.movie__form');
 
-          var form = $(this);
-          Helpers.changeButtonState(form, 'processing', true);
-          Elements.containers.movies.addClass('is-loading');
+        Helpers.changeButtonState(form, 'processing', true);
 
-          $.ajax({
-            url: form.attr('action'),
-            type: form.attr('method'),
-            data: form.serialize()
-          })
-          .always(function (data) {
-            data = data.responseJSON || data;
-            Elements.containers.unsubscribe.html(Helpers.compileTemplate('unsubscribe', data));
-            Elements.containers.notifications
-              .html(Helpers.compileTemplate('notifications', data))
-              .removeClass('is-loading');
-          });
+        $.ajax({
+          url: form.attr('action'),
+          type: form.attr('method'),
+          data: form.serialize()
+        })
+        .always(function (data) {
+          data = data.responseJSON || data;
+          container.html(Helpers.compileTemplate('subscribe', data));
         });
-      },
-
-      bindUnbscribeForm: function () {
-        Elements.body.on('submit', '.notification form', function (event) {
-          event.preventDefault();
-
-          var form = $(this);
-          Helpers.changeButtonState(form, 'processing', true);
-
-          $.ajax({
-            url: form.attr('action'),
-            type: form.attr('method'),
-            data: form.serialize()
-          })
-          .always(function (data) {
-            data = data.responseJSON || data;
-            Helpers.changeButtonState(form, 'primary', false);
-          });
-        });
-      }
+      });
     },
 
-    /**
-     * Features for Movies subscription
-     */
-    Movie: {
-      init: function () {
-        Elements.body.on('click', '.movie', function () {
-          var movie = $(this);
-          var data = {params: {movie_id: movie.data('id')}};
-          var container = movie.find('.movie__form');
+    bindTogglingMovieDetails: function () {
+      Elements.body.on('click', '.movie', function () {
+        var movie = $(this);
+        var data = {params: {movie_id: movie.data('id')}};
+        var container = movie.find('.movie__form');
 
-          if (!movie.hasClass('is-open')) {
-            movie.addClass('is-open');
-            container.html(Helpers.compileTemplate('subscribe', data));
-            container.find('input[name="email"]').attr('autofocus', true);
-
-            return false;
-          }
-        });
-
-        Elements.body.on('click', '[href="#show-details"]', function () {
-          var movie = $(this).closest('.movie');
-          movie.removeClass('is-open');
+        if (!movie.hasClass('is-open')) {
+          movie.addClass('is-open');
+          container.html(Helpers.compileTemplate('subscribe', data));
+          container.find('input[name="email"]').attr('autofocus', true);
 
           return false;
+        }
+      });
+
+      Elements.body.on('click', '[href="#show-details"]', function () {
+        var movie = $(this).closest('.movie');
+        movie.removeClass('is-open');
+
+        return false;
+      });
+    }
+
+  };
+
+  var Unsubscribe = {
+
+    init: function () {
+      if (Elements.containers.unsubscribe.length === 0) return;
+
+      Elements.containers.unsubscribe.html(Helpers.compileTemplate('unsubscribe'));
+      Unsubscribe.bindSearchForm();
+      Unsubscribe.bindUnbscribeForm();
+    },
+
+    bindSearchForm: function () {
+      Elements.body.on('submit', '#unsubscribe-container form', function (event) {
+        event.preventDefault();
+
+        var form = $(this);
+
+        Helpers.changeButtonState(form, 'processing', true);
+        Elements.containers.movies.addClass('is-loading');
+
+        $.ajax({
+          url: form.attr('action'),
+          type: form.attr('method'),
+          data: form.serialize()
+        })
+        .always(function (data) {
+          data = data.responseJSON || data;
+          Elements.containers.unsubscribe
+            .html(Helpers.compileTemplate('unsubscribe', data));
+          Elements.containers.notifications
+            .html(Helpers.compileTemplate('notifications', data))
+            .removeClass('is-loading');
         });
-      }
+      });
+    },
+
+    bindUnbscribeForm: function () {
+      Elements.body.on('submit', '.notification form', function (event) {
+        event.preventDefault();
+
+        var form = $(this);
+        var notification = form.closest('.notification');
+
+        Helpers.changeButtonState(form, 'processing', true);
+
+        $.ajax({
+          url: form.attr('action'),
+          type: form.attr('method'),
+          data: form.serialize()
+        })
+        .always(function (data) {
+          data = data.responseJSON || data;
+          Helpers.changeButtonState(form, 'primary', false);
+          notification.fadeOut(300, function() { $(this).remove(); });
+        });
+      });
+    }
+
+  };
+
+  var Application = {
+
+    init: function () {
+      Subscribe.init();
+      Unsubscribe.init();
     }
 
   };
