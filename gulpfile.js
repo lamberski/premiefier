7c8071810599d0d138a69c091a5da666dc42409c
Maciej Lamberski <maciej@lamberski.com>
2015-04-25 15:59:23 +0200

    Rename /src -> /source, /web -> /public

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,25 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    watch = require('gulp-watch');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var watch = require('gulp-watch');
 
 gulp.task('images', function() {
-  return gulp.src('src/images/*.{svg,jpg,png,gif}')
-    .pipe(gulp.dest('web/images'));
+  return gulp.src('source/images/**/*.{svg,jpg,png,gif}')
+    .pipe(gulp.dest('public/images'));
 });
 
 gulp.task('scripts', function() {
-  return gulp.src('src/scripts/*.js')
-    .pipe(gulp.dest('web/scripts'));
+  return gulp.src('source/scripts/**/*.js')
+    .pipe(gulp.dest('public/scripts'));
 });
 
 gulp.task('styles', function() {
-  return gulp.src('src/styles/*.scss')
+  return gulp.src('source/styles/**/*.scss')
     .pipe(sass())
     .pipe(autoprefixer())
-    .pipe(gulp.dest('web/styles'));
+    .pipe(gulp.dest('public/styles'));
 });
 
 gulp.task('default', function() {
-  gulp.watch('src/**/*', ['images', 'scripts', 'styles']);
+  gulp.watch('source/**/*', ['images', 'scripts', 'styles']);
 });
