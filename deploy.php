ee72478976d526d5c547c8ad5c819ad52767e0ff
Maciej Lamberski <maciej@lamberski.com>
2016-03-21 00:20:15 +0100

    Add deployment mechanism

---
+++ deploy.php
@@ -0,0 +1,16 @@
+<?php
+
+require 'recipe/composer.php';
+
+set('repository', 'https://github.com/lamberski/premiefier.git');
+set('default_stage', 'production');
+serverList('deploy.yml');
+
+task('deploy:assets', function () {
+    runLocally('gulp build');
+    foreach (glob('public/*/*') as $file) {
+        upload($file, '{{deploy_path}}/release/' . $file);
+    }
+})->desc('Compile and upload all static assets (JS, CSS, images)');
+
+before('deploy:symlink', 'deploy:assets');
