02de136dfc8038b8087b99494b9182df9b7b3d74
Maciej Lamberski <maciej@lamberski.com>
2016-03-21 09:53:13 +0100

    Strip away unnecessary comments

--- application/console.php
+++ application/console.php
@@ -1,13 +1,8 @@
 <?php
 
-// Include used classes
 use Premiefier\Commands\Notify;
 
-// Require bootstrap file
 $application = require_once __DIR__ . '/bootstrap.php';
 
-// Add available console commands
 $application['console']->add(new Notify());
-
-// Start the engine
 $application['console']->run();
